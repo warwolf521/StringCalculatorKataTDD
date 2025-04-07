1b385eb2b352d5f3c038d84812aad1f185108898
warwolf521 <bastiansanchez521@gmail.com>
2025-04-06 23:33:58 -0400

    add tests for handling negative numbers, failed run

--- test_Calculator.py
+++ test_Calculator.py
@@ -24,3 +24,21 @@ def test_add_step4():
     assert add("//;\n4;4;4") == 12
     assert add("//-\n8-10-12-14") == 44
     assert add("//?\n5?9?15?29") == 58
+
+def test_add_step5():
+    with pytest.raises(Exception) as excinfo:
+        add("-1,2")
+    assert str(excinfo.value) == "negatives not allowed: -1"
+
+    with pytest.raises(Exception) as excinfo:
+        add("4,-4,4")
+    assert str(excinfo.value) == "negatives not allowed: -4"
+
+    with pytest.raises(Exception) as excinfo:
+        add("-8,-10,12,-14")
+    assert str(excinfo.value) == "negatives not allowed: -8, -10, -14"
+
+    with pytest.raises(Exception) as excinfo:
+        add("//;\n-5;-9;15;-29")
+    assert str(excinfo.value) == "negatives not allowed: -5, -9, -29"
+    
