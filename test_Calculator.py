d0d1a988b0b8557e75aa7360d3d747bc16ed2150
warwolf521 <bastiansanchez521@gmail.com>
2025-04-06 23:11:31 -0400

    added third test and failed run

--- test_Calculator.py
+++ test_Calculator.py
@@ -12,3 +12,9 @@ def test_add_step2():
     assert add("8,10,12,14") == 44
     assert add("15,15,15,15,20") == 80
     assert add("4,4,4,7,32,73") == 124
+
+def test_add_step3():
+    assert add("1\n2") == 3
+    assert add("4\n4,4") == 12
+    assert add("8,10\n12,14") == 44
+    assert add("15\n15,30\n45") == 105
