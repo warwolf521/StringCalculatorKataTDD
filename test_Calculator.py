8577040ac4d399f156d3dad392021bb676a32e35
warwolf521 <bastiansanchez521@gmail.com>
2025-04-06 23:05:04 -0400

    added second failing test

--- test_Calculator.py
+++ test_Calculator.py
@@ -7,3 +7,8 @@ def test_add_step1():
     assert add("8,10") == 18
     assert add("15,15") == 30
 
+def test_add_step2():
+    assert add("1,2,3") == 6
+    assert add("4,4,4,7,32,73") == 124
+    assert add("8,10,12,14") == 44
+    assert add("15,15,15,15,20") == 80
